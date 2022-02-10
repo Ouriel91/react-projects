715e6c77af69509028792bc594b13b63dc3af3ba
Ouriel91 <orieloh91@gmail.com>
2022-02-10 13:54:06 +0200

    refactor code, add component and style

--- star-wars-home-task/src/App.js
+++ star-wars-home-task/src/App.js
@@ -1,69 +1,30 @@
 import React , {useEffect, useState} from 'react';
 import Bar from './Bar'
 import './App.css';
-
+import Table from '../components/Table'
 
 function App() {
 
-  const [vehicle, setVehicle] = useState({
-    pilots: []
-  })
+  
   
   const[planetsPopulation, setPlanetsPopulation] = useState([])
   const[largetPopulation, setLargetPopulation] = useState(0)
 
-  const getHeighestSumPopulationData = async() => {
-    const rawVehicles = await fetch('https://swapi.py4e.com/api/vehicles/')
-    const vehicles = await rawVehicles.json()
-
-    const resArr = vehicles.results
-    const fltArr = resArr.filter(res => res.pilots.length > 0)
-
-    for(let i = 0; i < fltArr.length; i++){
-        const vehicle = fltArr[i];
-        fltArr[i].population = 0
-
-        for(let j = 0; j < vehicle.pilots.length; j++){
-          const pilot = vehicle.pilots[j];
-          const details = await getPopulation(pilot);
-          fltArr[i].population += details.population 
-          vehicle.pilots[j] = details
-        }
-    } 
-
-    let maxIndex = 0
-    for(let i =0 ; i <fltArr.length; i++){
-      if(fltArr[i].population > fltArr[maxIndex].population){
-        maxIndex = i      
-      }
-    }
-
-    setVehicle(fltArr[maxIndex])
-  }
-
-  const getPopulation = async(pilotUrl) => {
-    const response = await fetch(pilotUrl)
-    const pilot = await response.json();
-
-    const response1 = await fetch(pilot.homeworld)
-    const world = await response1.json();
-
-    return {
-      population: +world.population, 
-      homeworld: world.name, 
-      pilotName: pilot.name
-    }
-  }
+  
 
   const getFivePlanetsPopulationData = async() => {
     const response = await fetch('https://swapi.py4e.com/api/planets/')
-    const data = await response.json()
+    const planetsData = await response.json()
 
+    getPlanetsDataAndMaxPopulation(planetsData)
+  }
+
+  const getPlanetsDataAndMaxPopulation = (planetsData) => {    
     const fiveStarsArr = ["Tatooine", "Alderaan", "Bespin", "Endor", "Naboo"]
     const retArr = []
 
     let max = 0
-    data.results.forEach(planet => {
+    planetsData.results.forEach(planet => {
       if(fiveStarsArr.includes(planet.name)){
         retArr.push({
           name:planet.name,
@@ -77,26 +38,16 @@ function App() {
     })
 
     setLargetPopulation(max)
-    setPlanetsPopulation(retArr) 
+    setPlanetsPopulation(retArr)
   }
 
   useEffect(() => {
-    getHeighestSumPopulationData()    
     getFivePlanetsPopulationData()
   },[])
 
   return (
     <div className="App">
-      <h2>Vehicle name with largest sum: {vehicle.name}</h2>
-      <div>
-        <h3>Related home planet and respective population: </h3>
-        {vehicle.pilots.map((pilot) => <p key={pilot.pilotName}>{pilot.homeworld}, {pilot.population}</p>)} 
-      </div>
-      <div>
-        <h3>Related pilot names:</h3>
-        {vehicle.pilots.map((pilot) => <span key={pilot.pilotName}>{pilot.pilotName} </span>)}
-      </div>
-
+      <Table />
       <div style={{height: '40vh', display: 'flex', verticalAlign: 'top'}}>
         <Bar population={100} height={'50%'} planetName="bla"/>
         <Bar population={102} height={'20%'} planetName="bla"/>
---
+++ star-wars-home-task/src/components/Table.js
@@ -0,0 +1,85 @@
+import React, {useState, useEffect} from 'react';
+import '../styles/Table.css'
+
+function Table() {
+
+    const [vehicle, setVehicle] = useState({
+        pilots: []
+    })
+
+    const getHeighestSumPopulationData = async() => {
+        const responseVehicles = await fetch('https://swapi.py4e.com/api/vehicles/')
+        const vehicles = await responseVehicles.json()
+    
+        const fliteredPilotsArr = 
+          vehicles.results.filter(res => res.pilots.length > 0)
+    
+        for(let i = 0; i < fliteredPilotsArr.length; i++){
+            const pilotObj = fliteredPilotsArr[i];
+            pilotObj.population = 0 //initial for summarize all population in home planet's pilot
+    
+            for(let j = 0; j < pilotObj.pilots.length; j++){
+              const pilot = pilotObj.pilots[j];
+              const pilotDetails = await getPopulation(pilot);
+              pilotObj.population += pilotDetails.population //calculate population
+              pilotObj.pilots[j] = pilotDetails //update original pilots array, object instead link
+            }
+        } 
+    
+        const maxIndex = getMaxPopulationIndex(fliteredPilotsArr)
+      
+        setVehicle(fliteredPilotsArr[maxIndex])
+      }
+    
+      const getPopulation = async(pilotUrl) => {
+        const responsePilot = await fetch(pilotUrl)
+        const pilot = await responsePilot.json();
+    
+        const responseHomeWorld = await fetch(pilot.homeworld)
+        const world = await responseHomeWorld.json();
+    
+        return {
+          population: +world.population, 
+          homeworld: world.name, 
+          pilotName: pilot.name
+        }
+      }
+    
+      const getMaxPopulationIndex = (fliteredPilotsArr) => {
+        let maxIndex = 0
+    
+        for(let i =0 ; i <fliteredPilotsArr.length; i++){
+          if(fliteredPilotsArr[i].population > fliteredPilotsArr[maxIndex].population){
+            maxIndex = i      
+          }
+        }
+    
+        return maxIndex
+      }
+
+    useEffect(() => {
+        getHeighestSumPopulationData()    
+    },[])  
+
+  return (
+    <div className="table"> 
+        <div className="table-row">
+        <h3>
+            Vehicle name with largest sum: <span className="details-text">{vehicle.name}</span>
+        </h3>
+        </div>
+        <div className="table-row">
+        <h3>Related home planet and respective population: </h3>
+        {vehicle.pilots.map((pilot) => 
+            <span className="details-text" key={pilot.pilotName}>{pilot.homeworld}: {pilot.population}.  </span>)} 
+        </div>
+        <div>
+        <h3>Related pilot names:</h3>
+        {vehicle.pilots.map((pilot) => 
+            <span className="details-text" key={pilot.pilotName}>{pilot.pilotName} </span>)}
+        </div>
+    </div>
+  );
+}
+
+export default Table;
